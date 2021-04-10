e2a8b06966a3079d6ba22b0ae6b865aca23b77d8
Hakim Rachidi <hakim.rachidi@gym-papenburg.de>
2021-04-10 12:25:30 +0200

    REST call to rasa service

--- chat/app.js
+++ chat/app.js
@@ -1,9 +1,16 @@
 const express = require("express")
+const axios = require("axios")
 
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello from /chat")
+    res.send("Hello from /chat\n")
+    axios.post("rasa-x-rasa-production.rasa:5005/webhooks/rest/webhook", {
+        sender: "hakim",
+        message: "Hello"
+    }).then(d => {
+        res.send(d)
+    })
 })
 
 app.listen(80, () => {
