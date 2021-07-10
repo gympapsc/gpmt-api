f50f28d1c6650871676964ad8c66591a0078ba39
hakrac <hakimrachidi02@gmail.com>
2021-07-10 11:26:27 +0200

    Update Photo model

--- src/models/photo.js
+++ src/models/photo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const photoSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String },
     user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" }
 }, {
     timestamps: {
